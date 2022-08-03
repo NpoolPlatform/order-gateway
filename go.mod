470da4d13a8993752f5094909e2d525378e00ff1
Zhao KK <kikakkz@hotmail.com>
2022-08-03 15:20:15 +0000

    Add query

--- go.mod
+++ go.mod
@@ -12,9 +12,9 @@ require (
 	github.com/NpoolPlatform/inspire-middleware v0.0.0-20220802143144-ce9626252b91
 	github.com/NpoolPlatform/ledger-manager v0.0.0-20220801083606-458fa4cd2fbe
 	github.com/NpoolPlatform/libent-cruder v0.0.0-20220801075201-cab5db8b6290
-	github.com/NpoolPlatform/message v0.0.0-20220803143209-258593bd4452
+	github.com/NpoolPlatform/message v0.0.0-20220803151244-c47d5b6beb8d
 	github.com/NpoolPlatform/oracle-manager v0.0.0-20220615040511-dc235b31fa14
-	github.com/NpoolPlatform/order-middleware v0.0.0-20220803135921-d59b66e48316
+	github.com/NpoolPlatform/order-middleware v0.0.0-20220803145646-34ea3bebe402
 	github.com/NpoolPlatform/sphinx-coininfo v0.0.0-20220731103603-21004f4341dc
 	github.com/NpoolPlatform/sphinx-proxy v0.0.0-20220803040820-2b7bd4158e8b
 	github.com/NpoolPlatform/staker-manager v0.0.0-20220715052242-57491c7251b9
--- pkg/order/query.go
+++ pkg/order/query.go
@@ -2,13 +2,23 @@ package order
 
 import (
 	"context"
+	"fmt"
 
+	usercli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
+	billingcli "github.com/NpoolPlatform/cloud-hashing-billing/pkg/client"
+	goodcli "github.com/NpoolPlatform/cloud-hashing-goods/pkg/client"
+	couponcli "github.com/NpoolPlatform/inspire-middleware/pkg/client/coupon"
 	npool "github.com/NpoolPlatform/message/npool/order/gw/v1/order"
-	ordermwcli "github.com/NpoolPlatform/order-middleware/pkg/client/order"
+	ordercli "github.com/NpoolPlatform/order-middleware/pkg/client/order"
+	coininfocli "github.com/NpoolPlatform/sphinx-coininfo/pkg/client"
+
+	couponpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/inspire/coupon"
+
+	"github.com/shopspring/decimal"
 )
 
-func GetOrder(ctx context.Context, id string) (*npool.Order, error) {
-	ord, err := ordermwcli.GetOrder(ctx, id)
+func GetOrder(ctx context.Context, id string) (*npool.Order, error) { //nolint
+	ord, err := ordercli.GetOrder(ctx, id)
 	if err != nil {
 		return nil, err
 	}
@@ -38,6 +48,96 @@ func GetOrder(ctx context.Context, id string) (*npool.Order, error) {
 		CreatedAt: ord.CreatedAt,
 		PaidAt:    ord.PaidAt,
 		State:     ord.State,
+
+		Start: ord.Start,
+		End:   ord.End,
+	}
+
+	user, err := usercli.GetUser(ctx, ord.AppID, ord.UserID)
+	if err != nil {
+		return nil, err
+	}
+	if user == nil {
+		return nil, fmt.Errorf("invalid user")
+	}
+
+	o.EmailAddress = user.EmailAddress
+	o.PhoneNO = user.PhoneNO
+
+	good, err := goodcli.GetGood(ctx, ord.GoodID)
+	if err != nil {
+		return nil, err
+	}
+	if good == nil {
+		return nil, fmt.Errorf("invalid good")
+	}
+
+	o.GoodName = good.Title
+
+	coin, err := coininfocli.GetCoinInfo(ctx, good.CoinInfoID)
+	if err != nil {
+		return nil, err
+	}
+	if coin == nil {
+		return nil, fmt.Errorf("invalid good coin")
+	}
+
+	o.CoinTypeID = good.CoinInfoID
+	o.CoinName = coin.Name
+	o.CoinLogo = coin.Logo
+	o.CoinUnit = coin.Unit
+
+	coin, err = coininfocli.GetCoinInfo(ctx, ord.PaymentCoinTypeID)
+	if err != nil {
+		return nil, err
+	}
+	if coin == nil {
+		return nil, fmt.Errorf("invalid payment coin")
+	}
+
+	o.PaymentCoinName = coin.Name
+	o.PaymentCoinLogo = coin.Logo
+	o.PaymentCoinUnit = coin.Unit
+
+	account, err := billingcli.GetAccount(ctx, ord.PaymentAccountID)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: for old placeholder payment
+	if account == nil {
+		return nil, fmt.Errorf("invalid account")
+	}
+
+	o.PaymentAddress = account.Address
+
+	coupon, err := couponcli.GetCoupon(ctx, ord.FixAmountID, couponpb.CouponType_FixAmount)
+	if err != nil {
+		return nil, err
+	}
+	if coupon != nil {
+		o.FixAmountName = coupon.Name
+		o.FixAmountAmount = coupon.Value
+	}
+
+	coupon, err = couponcli.GetCoupon(ctx, ord.DiscountID, couponpb.CouponType_Discount)
+	if err != nil {
+		return nil, err
+	}
+	if coupon != nil {
+		o.DiscountName = coupon.Name
+		v, err := decimal.NewFromString(coupon.Value)
+		if err != nil {
+			return nil, err
+		}
+		o.DiscountPercent = uint32(v.IntPart())
+	}
+
+	coupon, err = couponcli.GetCoupon(ctx, ord.SpecialOfferID, couponpb.CouponType_SpecialOffer)
+	if err != nil {
+		return nil, err
+	}
+	if coupon != nil {
+		o.SpecialOfferAmount = coupon.Value
 	}
 
 	return o, nil
