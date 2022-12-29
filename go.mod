c596fd631223faed4bab33fae6eb655c5f72ea79
Zhao KK <kikakkz@hotmail.com>
2022-12-29 02:34:19 +0000

    Remove migration

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/NpoolPlatform/order-gateway
 go 1.17
 
 require (
-	entgo.io/ent v0.11.3
 	github.com/NpoolPlatform/account-middleware v0.0.0-20221216032556-c0953cebb9a4
 	github.com/NpoolPlatform/api-manager v0.0.0-20220826022307-d72866fe3ec0
 	github.com/NpoolPlatform/appuser-middleware v0.0.0-20220828053822-e809766566c6
@@ -34,7 +33,7 @@ require (
 
 require (
 	ariga.io/atlas v0.7.2-0.20220927111110-867ee0cca56a // indirect
-	github.com/NpoolPlatform/cloud-hashing-inspire v0.0.0-20220731100543-621262acb09b // indirect
+	entgo.io/ent v0.11.3 // indirect
 	github.com/Shonminh/apollo-client v0.4.0 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -1,44 +1,29 @@
-//nolint:nolintlint
+//nolint
 package migrator
 
 import (
 	"context"
 	"database/sql"
 	"fmt"
-
-	"entgo.io/ent/dialect"
-	"github.com/shopspring/decimal"
-
-	"github.com/google/uuid"
-
-	entsql "entgo.io/ent/dialect/sql"
-
-	cconst "github.com/NpoolPlatform/cloud-hashing-order/pkg/message/const"
+	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
-	corderent "github.com/NpoolPlatform/cloud-hashing-order/pkg/db/ent"
-
-	"time"
-
 	constant "github.com/NpoolPlatform/go-service-framework/pkg/mysql/const"
-	"github.com/NpoolPlatform/order-manager/pkg/db"
-	"github.com/NpoolPlatform/order-manager/pkg/db/ent"
 )
 
 func Migrate(ctx context.Context) error {
-	return migrateFinishAmount(ctx)
+	return nil
 }
 
 const (
-	keyUsername  = "username"
-	keyPassword  = "password"
-	keyDBName    = "database_name"
-	maxOpen      = 10
-	maxIdle      = 10
-	MaxLife      = 3
-	priceScale12 = 1000000000000
+	keyUsername = "username"
+	keyPassword = "password"
+	keyDBName   = "database_name"
+	maxOpen     = 10
+	maxIdle     = 10
+	MaxLife     = 3
 )
 
 func dsn(hostname string) (string, error) {
@@ -79,75 +64,4 @@ func open(hostname string) (conn *sql.DB, err error) {
 	conn.SetMaxIdleConns(maxIdle)
 
 	return conn, nil
-}
-
-//nolint
-func migrateFinishAmount(ctx context.Context) (err error) {
-	cloudOrder, err := open(cconst.ServiceName)
-	if err != nil {
-		return err
-	}
-	defer cloudOrder.Close()
-
-	cloudOrderCli := corderent.NewClient(corderent.Driver(entsql.OpenDB(dialect.MySQL, cloudOrder)))
-
-	logger.Sugar().Infow("Migrate order", "Start", "...")
-
-	defer func() {
-		logger.Sugar().Infow("Migrate order", "Done", "...", "error", err)
-	}()
-
-	amount := func(samount uint64) decimal.Decimal {
-		if samount == 0 {
-			return decimal.NewFromInt(0)
-		}
-		return decimal.NewFromInt(int64(samount)).Div(decimal.NewFromInt(priceScale12))
-	}
-
-	paymentInfos, err := cloudOrderCli.
-		Payment.
-		Query().
-		All(ctx)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	paymentMap := map[uuid.UUID]*corderent.Payment{}
-	for _, payment := range paymentInfos {
-		paymentMap[payment.OrderID] = payment
-	}
-
-	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		infos, err := tx.
-			Payment.
-			Query().
-			All(_ctx)
-		if err != nil {
-			return err
-		}
-
-		for _, info := range infos {
-			payment, ok := paymentMap[info.OrderID]
-			if !ok {
-				continue
-			}
-
-			_, err := tx.
-				Payment.
-				UpdateOneID(info.ID).
-				SetFinishAmount(amount(payment.FinishAmount)).
-				Save(_ctx)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
