ee7afd243dc48b8a08bddd1bac38f5b74c55ea6d
Zhao KK <kikakkz@hotmail.com>
2024-04-25 11:31:10 +0000

    Add out of gases api

---
+++ pkg/outofgas/handler.go
@@ -0,0 +1,122 @@
+package outofgas
+
+import (
+	"context"
+	"fmt"
+
+	constant "github.com/NpoolPlatform/order-gateway/pkg/const"
+	ordercommon "github.com/NpoolPlatform/order-gateway/pkg/order/common"
+)
+
+type Handler struct {
+	ordercommon.OrderCheckHandler
+	Offset int32
+	Limit  int32
+}
+
+func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
+	handler := &Handler{}
+	for _, opt := range options {
+		if err := opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithAppID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid appid")
+			}
+			return nil
+		}
+		if err := h.CheckAppWithAppID(ctx, *id); err != nil {
+			return err
+		}
+		h.AppID = id
+		return nil
+	}
+}
+
+func WithUserID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid userid")
+			}
+			return nil
+		}
+		if err := h.CheckUserWithUserID(ctx, *id); err != nil {
+			return err
+		}
+		h.UserID = id
+		return nil
+	}
+}
+
+func WithGoodID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid goodid")
+			}
+			return nil
+		}
+		if err := h.CheckGoodWithGoodID(ctx, *id); err != nil {
+			return err
+		}
+		h.GoodID = id
+		return nil
+	}
+}
+
+func WithAppGoodID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid appgoodid")
+			}
+			return nil
+		}
+		if err := h.CheckAppGoodWithAppGoodID(ctx, *id); err != nil {
+			return err
+		}
+		h.AppGoodID = id
+		return nil
+	}
+}
+
+func WithOrderID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid orderid")
+			}
+			return nil
+		}
+		if err := h.CheckOrderWithOrderID(ctx, *id); err != nil {
+			return err
+		}
+		h.OrderID = id
+		return nil
+	}
+}
+
+func WithOffset(offset int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = offset
+		return nil
+	}
+}
+
+func WithLimit(limit int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if limit == 0 {
+			limit = constant.DefaultRowLimit
+		}
+		h.Limit = limit
+		return nil
+	}
+}
---
+++ pkg/outofgas/query.go
@@ -0,0 +1,131 @@
+package outofgas
+
+import (
+	"context"
+
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	appmwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/app"
+	usermwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	appgoodmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/app/good"
+	npool "github.com/NpoolPlatform/message/npool/order/gw/v1/outofgas"
+	outofgasmwpb "github.com/NpoolPlatform/message/npool/order/mw/v1/outofgas"
+	ordergwcommon "github.com/NpoolPlatform/order-gateway/pkg/common"
+	outofgasmwcli "github.com/NpoolPlatform/order-middleware/pkg/client/outofgas"
+)
+
+type queryHandler struct {
+	*Handler
+	outOfGases []*outofgasmwpb.OutOfGas
+	infos      []*npool.OutOfGas
+	apps       map[string]*appmwpb.App
+	users      map[string]*usermwpb.User
+	appGoods   map[string]*appgoodmwpb.Good
+}
+
+func (h *queryHandler) getApps(ctx context.Context) (err error) {
+	h.apps, err = ordergwcommon.GetApps(ctx, func() (appIDs []string) {
+		for _, outOfGas := range h.outOfGases {
+			appIDs = append(appIDs, outOfGas.AppID)
+		}
+		return
+	}())
+	return err
+}
+
+func (h *queryHandler) getUsers(ctx context.Context) (err error) {
+	h.users, err = ordergwcommon.GetUsers(ctx, func() (userIDs []string) {
+		for _, outOfGas := range h.outOfGases {
+			userIDs = append(userIDs, outOfGas.UserID)
+		}
+		return
+	}())
+	return err
+}
+
+func (h *queryHandler) getAppGoods(ctx context.Context) (err error) {
+	h.appGoods, err = ordergwcommon.GetAppGoods(ctx, func() (appGoodIDs []string) {
+		for _, outOfGas := range h.outOfGases {
+			appGoodIDs = append(appGoodIDs, outOfGas.AppGoodID)
+		}
+		return
+	}())
+	return err
+}
+
+func (h *queryHandler) formalize() {
+	for _, outOfGas := range h.outOfGases {
+		app, ok := h.apps[outOfGas.AppID]
+		if !ok {
+			continue
+		}
+		user, ok := h.users[outOfGas.UserID]
+		if !ok {
+			continue
+		}
+		appGood, ok := h.appGoods[outOfGas.AppGoodID]
+		if !ok {
+			continue
+		}
+		h.infos = append(h.infos, &npool.OutOfGas{
+			ID:           outOfGas.ID,
+			EntID:        outOfGas.EntID,
+			AppID:        outOfGas.AppID,
+			AppName:      app.Name,
+			UserID:       outOfGas.UserID,
+			EmailAddress: user.EmailAddress,
+			PhoneNO:      user.PhoneNO,
+			GoodID:       outOfGas.GoodID,
+			GoodType:     outOfGas.GoodType,
+			GoodName:     appGood.GoodName,
+			AppGoodID:    outOfGas.AppGoodID,
+			AppGoodName:  appGood.AppGoodName,
+			OrderID:      outOfGas.OrderID,
+			StartAt:      outOfGas.StartAt,
+			EndAt:        outOfGas.EndAt,
+			CreatedAt:    outOfGas.CreatedAt,
+			UpdatedAt:    outOfGas.UpdatedAt,
+		})
+	}
+}
+
+func (h *Handler) GetOutOfGass(ctx context.Context) ([]*npool.OutOfGas, uint32, error) {
+	conds := &outofgasmwpb.Conds{}
+	if h.AppID != nil {
+		conds.AppID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.AppID}
+	}
+	if h.UserID != nil {
+		conds.UserID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.UserID}
+	}
+	if h.GoodID != nil {
+		conds.GoodID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.GoodID}
+	}
+	if h.AppGoodID != nil {
+		conds.AppGoodID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.AppGoodID}
+	}
+	if h.OrderID != nil {
+		conds.OrderID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.OrderID}
+	}
+
+	infos, total, err := outofgasmwcli.GetOutOfGases(ctx, conds, h.Offset, h.Limit)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	handler := &queryHandler{
+		Handler:    h,
+		outOfGases: infos,
+	}
+	if err := handler.getApps(ctx); err != nil {
+		return nil, 0, err
+	}
+	if err := handler.getUsers(ctx); err != nil {
+		return nil, 0, err
+	}
+	if err := handler.getAppGoods(ctx); err != nil {
+		return nil, 0, err
+	}
+
+	handler.formalize()
+	return handler.infos, total, nil
+}
