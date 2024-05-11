ff009c4d6597f0681ad525db65020ef67838b74c
Zhao KK <kikakkz@hotmail.com>
2024-05-11 09:39:43 +0000

    Add compensate apis

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 
 	order "github.com/NpoolPlatform/message/npool/order/gw/v1"
 	appconfig1 "github.com/NpoolPlatform/order-gateway/api/app/config"
+	compensate1 "github.com/NpoolPlatform/order-gateway/api/compensate"
 	feeorder1 "github.com/NpoolPlatform/order-gateway/api/fee"
 	powerrentalorder1 "github.com/NpoolPlatform/order-gateway/api/powerrental"
 
@@ -19,6 +20,7 @@ type Server struct {
 func Register(server grpc.ServiceRegistrar) {
 	order.RegisterGatewayServer(server, &Server{})
 	feeorder1.Register(server)
+	compensate1.Register(server)
 	powerrentalorder1.Register(server)
 	appconfig1.Register(server)
 }
@@ -28,6 +30,9 @@ func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOpt
 		return err
 	}
 	if err := feeorder1.RegisterGateway(mux, endpoint, opts); err != nil {
+		return err
+	}
+	if err := compensate1.RegisterGateway(mux, endpoint, opts); err != nil {
 		return err
 	}
 	if err := powerrentalorder1.RegisterGateway(mux, endpoint, opts); err != nil {
