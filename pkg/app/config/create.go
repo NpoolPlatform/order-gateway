2eafb7e01958f7296d46f8d4c50ec4d93c110d8d
Zhao KK <kikakkz@hotmail.com>
2024-06-04 09:12:55 +0000

    Fix: http://47.116.78.131:10083/issues/3579

--- pkg/app/config/create.go
+++ pkg/app/config/create.go
@@ -5,9 +5,14 @@ import (
 
 	appconfigmwpb "github.com/NpoolPlatform/message/npool/order/mw/v1/app/config"
 	appconfigmwcli "github.com/NpoolPlatform/order-middleware/pkg/client/app/config"
+
+	"github.com/google/uuid"
 )
 
 func (h *Handler) CreateAppConfig(ctx context.Context) (*appconfigmwpb.AppConfig, error) {
+	if h.EntID == nil {
+		h.EntID = func() *string { s := uuid.NewString(); return &s }()
+	}
 	if err := appconfigmwcli.CreateAppConfig(ctx, &appconfigmwpb.AppConfigReq{
 		AppID:                                  h.AppID,
 		EnableSimulateOrder:                    h.EnableSimulateOrder,
