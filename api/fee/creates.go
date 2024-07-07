5dba561ea47f3ab1ea759527560533e86d03f191
Zhao KK <kikakkz@hotmail.com>
2024-07-07 05:03:13 +0000

    Fix: http://47.116.78.131:10083/issues/3872

--- api/fee/creates.go
+++ api/fee/creates.go
@@ -5,7 +5,6 @@ import (
 
 	types "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
 	npool "github.com/NpoolPlatform/message/npool/order/gw/v1/fee"
-	ordercommon "github.com/NpoolPlatform/order-gateway/api/order/common"
 	feeorder1 "github.com/NpoolPlatform/order-gateway/pkg/fee"
 
 	"google.golang.org/grpc/codes"
@@ -48,48 +47,6 @@ func (s *Server) CreateFeeOrders(ctx context.Context, in *npool.CreateFeeOrdersR
 	}
 
 	return &npool.CreateFeeOrdersResponse{
-		Infos: infos,
-	}, nil
-}
-
-func (s *Server) CreateUserFeeOrders(ctx context.Context, in *npool.CreateUserFeeOrdersRequest) (*npool.CreateUserFeeOrdersResponse, error) {
-	if err := ordercommon.ValidateAdminCreateOrderType(in.GetOrderType()); err != nil {
-		logger.Sugar().Errorw(
-			"CreateUserFeeOrders",
-			"In", in,
-		)
-		return &npool.CreateUserFeeOrdersResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	handler, err := feeorder1.NewHandler(
-		ctx,
-		feeorder1.WithAppID(&in.AppID, true),
-		feeorder1.WithUserID(&in.TargetUserID, true),
-		feeorder1.WithAppGoodIDs(in.AppGoodIDs, true),
-		feeorder1.WithParentOrderID(&in.ParentOrderID, true),
-		feeorder1.WithOrderType(&in.OrderType, true),
-		feeorder1.WithCreateMethod(func() *types.OrderCreateMethod { e := types.OrderCreateMethod_OrderCreatedByAdmin; return &e }(), true),
-		feeorder1.WithDurationSeconds(&in.DurationSeconds, true),
-	)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"CreateUserFeeOrders",
-			"In", in,
-			"Error", err,
-		)
-		return &npool.CreateUserFeeOrdersResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-
-	infos, err := handler.CreateFeeOrders(ctx)
-	if err != nil {
-		logger.Sugar().Errorw(
-			"CreateUserFeeOrders",
-			"In", in,
-			"Error", err,
-		)
-		return &npool.CreateUserFeeOrdersResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	return &npool.CreateUserFeeOrdersResponse{
 		Infos: infos,
 	}, nil
 }
